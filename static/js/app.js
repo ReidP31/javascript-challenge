8e52511d0438c464d9d48045108aebd7437b16cf
Reid Powell <reid.e.powell@gmail.com>
2019-11-22 22:33:24 -0500

    added comment for table function

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ var tableData = data;
 // Grab reference to the table body
 var tbody = d3.select("tbody");
 
-
+// Add rows and cells to the table, text to the cells
 data.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(([key, value]) => {
