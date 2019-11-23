1fa734cd2164e415950b20dec1b7b142d664f947
Reid Powell <reid.e.powell@gmail.com>
2019-11-22 22:30:54 -0500

    added app script to append rows and cells to tbody

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Grab reference to the table body
+var tbody = d3.select("tbody");
+
+
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
